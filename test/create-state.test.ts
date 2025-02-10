004d6b616a1da1348afde5bdd91273ea70c4af1d
iobuhov <vyusfire@gmail.com>
2025-02-10 17:25:07 +0100

    add create-state test

--- test/create-state.test.ts
+++ test/create-state.test.ts
@@ -1,7 +1,37 @@
-import { describe, test, expect, beforeEach } from 'vitest';
+import { autorun, observable } from 'mobx';
+import { proxy } from 'valtio/vanilla';
+import { watch } from 'valtio/vanilla/utils';
+import { describe, it, expect, beforeEach, assert } from 'vitest';
 
 describe('mobx', () => {
   describe('observable', () => {
-    test('should create');
+    it('should create observable', () => {
+      let count = 0;
+      let state = observable({ size: 4 });
+      autorun(() => {
+        state.size;
+        ++count;
+      });
+      expect(count).toBe(1)
+      state.size += 1;
+      expect(count).toBe(2)
+    });
   });
 });
+
+describe("valtio", () => {
+  describe("proxy", () => {
+    it("should create observable", () => {
+      let count = 0;
+      let state = proxy({ size: 4 });
+      watch(get => {
+        get(state);
+        ++count;
+      }, { sync: true })
+
+      expect(count).toBe(1);
+      state.size += 1;
+      expect(count).toBe(2);
+    })
+  })
+})
